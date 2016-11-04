f89750704018b35d9e936588049af98de473d1df
Minsuk Eom <boxresin@gmail.com>
2016-11-04 23:29:53 +0900

    세팅 액티비티 추가

---
+++ app/src/main/java/app/boxresin/runninghealthapp/SettingActivity.java
@@ -0,0 +1,19 @@
+package app.boxresin.runninghealthapp;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by eomin on 2016-11-04.
+ */
+
+public class SettingActivity extends AppCompatActivity
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+	{
+		super.onCreate(savedInstanceState, persistentState);
+		setContentView(R.layout.activity_setting);
+	}
+}
