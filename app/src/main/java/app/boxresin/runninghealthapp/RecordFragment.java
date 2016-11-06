f770d1ab257d3c4034db9ded432f3276fe267c5f
Minsuk Eom <boxresin@gmail.com>
2016-11-06 22:59:54 +0900

    기록 프래그먼트 완성

--- app/src/main/java/app/boxresin/runninghealthapp/RecordFragment.java
+++ app/src/main/java/app/boxresin/runninghealthapp/RecordFragment.java
@@ -1,10 +1,68 @@
 package app.boxresin.runninghealthapp;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.Locale;
+
+import app.boxresin.runninghealthapp.databinding.FragmentRecordBinding;
+import app.boxresin.runninghealthapp.databinding.ItemRecordBinding;
+import data.Record;
 
 /**
  * 기록 프래그먼트
  */
 public class RecordFragment extends Fragment
 {
+	private FragmentRecordBinding binding;
+	private ArrayAdapter<Record> adapter;
+
+	@Nullable
+	@Override
+	public View onCreateView(final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+	{
+		binding = DataBindingUtil.inflate(inflater, R.layout.fragment_record, container, false);
+
+		adapter = new ArrayAdapter<Record>(getContext(), R.layout.item_record)
+		{
+			@NonNull
+			@Override
+			public View getView(int position, View convertView, ViewGroup parent)
+			{
+				ItemRecordBinding binding = null;
+
+				if (convertView == null)
+				{
+					binding = DataBindingUtil.inflate(inflater, R.layout.item_record, parent, false);
+					convertView = binding.getRoot();
+				}
+				else
+					binding = DataBindingUtil.getBinding(convertView);
+
+				Record data = getItem(position);
+				binding.txtRecordName.setText(data.getName());
+				binding.txtDate.setText(data.getDate() + " 종료");
+				binding.txtElasped.setText(String.format(Locale.KOREAN, "%.1f분 동안", data.getElapsed()));
+				binding.txtMoved.setText(String.format(Locale.KOREAN, "총 %.2f ㎞ 이동", data.getMoved()));
+				binding.txtConsumed.setText(String.format(Locale.KOREAN, "총 %.2f ㎉ 소모", data.getConsumed()));
+
+				return convertView;
+			}
+		};
+		binding.listRecord.setAdapter(adapter);
+
+		// NOTE 더미 데이터 추가. 나중에 지울 것
+		adapter.add(new Record("한강 산책", 1.625, 310, 12.6));
+		adapter.add(new Record("여의도 탐험", 3.25, 598.2, 28.1));
+		adapter.add(new Record("어쩌구 산책", 0.43, 94.7, 7.3));
+
+		return binding.getRoot();
+	}
 }
--- app/src/main/java/data/Record.java
+++ app/src/main/java/data/Record.java
@@ -9,16 +9,18 @@ import java.util.Locale;
 public class Record
 {
 	private String name; // 기록 이름
-	private String date; // 기록 날짜
+	private String date; // 기록 날짜 (운동을 끝낸 시간)
 	private double moved; // 총 움직인 거리
 	private double consumed; // 총 소비한 칼로리
+	private double elapsed; // 총 운동 시간
 
-	public Record(String name, double moved, double consumed)
+	public Record(String name, double moved, double consumed, double elapsed)
 	{
+		this.elapsed = elapsed;
 		Calendar today = Calendar.getInstance();
 
 		this.name = name;
-		this.date = String.format(Locale.KOREAN, "%d-%d-%d\n%d시 %d분",
+		this.date = String.format(Locale.KOREAN, "%04d-%02d-%02d  %d시 %d분",
 				today.get(Calendar.YEAR), today.get(Calendar.MONTH) + 1, today.get(Calendar.DAY_OF_MONTH),
 				today.get(Calendar.HOUR_OF_DAY), today.get(Calendar.MINUTE));
 		this.moved = moved;
@@ -63,5 +65,15 @@ public class Record
 	public void setConsumed(double consumed)
 	{
 		this.consumed = consumed;
+	}
+
+	public double getElapsed()
+	{
+		return elapsed;
+	}
+
+	public void setElapsed(double elapsed)
+	{
+		this.elapsed = elapsed;
 	}
 }
